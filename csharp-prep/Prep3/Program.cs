a7751c59def5442bb92d5417c6726d0c6eeb1e97
Blackpride15 <asu23006@byui.edu>
2024-06-15 22:40:51 +0100

    prep 1-3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicNumber = Convert.ToInt32(Console.ReadLine());
+
+        Random random = new Random();
+        int guess = 0; 
+        while (guess != magicNumber)
+        {
+            // Ask for the user's guess
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            
+            // Check if the guess is higher, lower, or correct
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
